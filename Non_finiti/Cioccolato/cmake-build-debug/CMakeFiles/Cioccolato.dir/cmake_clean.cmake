3e1f24ee903f7d73b4015dd9bcfa231947f225f4
jaupi-enrico <78313537@itiscastelli.onmicrosoft.com>
2025-02-03 08:47:33 +0100

    Relocation

---
+++ Non_finiti/Cioccolato/cmake-build-debug/CMakeFiles/Cioccolato.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cioccolato.dir/main.cpp.obj"
+  "CMakeFiles/Cioccolato.dir/main.cpp.obj.d"
+  "Cioccolato.exe"
+  "Cioccolato.exe.manifest"
+  "Cioccolato.pdb"
+  "libCioccolato.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cioccolato.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Non_finiti/Cioccolato/cmake-build-debug/CMakeFiles/Cioccolato.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Cioccolato.
+# This may be replaced when dependencies are built.
