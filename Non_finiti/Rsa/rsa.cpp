b5ad3db9df3d31718d9da362ce06498577b0323f
jaupi-enrico <78313537@itiscastelli.onmicrosoft.com>
2025-02-03 09:00:49 +0100

    Modifica struttura di calcolo non finita

--- Non_finiti/Rsa/rsa.cpp
+++ Non_finiti/Rsa/rsa.cpp
@@ -1,3 +1,20 @@
+long long int power()
+{
+    long long int num = 1, A, B, AB;
+    if (d == 1)
+    {
+        num = messaggio[i];
+    }
+    A = messaggio[i];
+    B = messaggio[i] % N;
+    for (int j = 0; j < d - 1; ++j)
+    {
+        A = (A * B) % N;
+    }
+    num = A % N;
+    return num;
+}
+
 void decifra(int N, int d, int L, int* messaggio, char* plaintext) {
     // Esempio di implementazione con un solo carattere da decifrare.
     // Mettiamo il risultato della decifrazione in plaintext[0] e il
@@ -5,18 +22,6 @@ void decifra(int N, int d, int L, int* messaggio, char* plaintext) {
 
     for (int i = 0; i < L; ++i)
     {
-        long long int num = 1, A, B, AB;
-        if (d == 1)
-        {
-            num = messaggio[i];
-        }
-        A = messaggio[i];
-        B = messaggio[i] % N;
-        for (int j = 0; j < d - 1; ++j)
-        {
-            A = (A * B) % N;
-        }
-        num = A % N;
         plaintext[i] = num;
     }
     plaintext[L] = '\0';
